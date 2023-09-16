c094e4941921f2d412a8020b46a2040941c6a198
ritik <ritik.programmer@gmail.com>
2023-09-17 00:57:33 +0530

    amends

--- Unzipper.py
+++ Unzipper.py
@@ -1,17 +1,12 @@
 import zipfile
 import os
 
-# folder_path = 'E:/Dataset/'
-# destination_path = 'E:/Dataset_Unzipped/'
-# for filename in os.listdir(folder_path):
-#     if filename.endswith('.zip'):
-#         with zipfile.ZipFile(os.path.join(folder_path, filename), 'r') as zip_ref:
-#             zip_ref.extractall(destination_path)
+folder_path = "E:/Dataset/"
+destination_path = "E:/Dataset_Unzipped/"
+for filename in os.listdir(folder_path):
+    if filename.endswith(".zip"):
+        with zipfile.ZipFile(os.path.join(folder_path, filename), "r") as zip_ref:
+            zip_ref.extractall(destination_path)
+        print("The zip file " + filename + " has been extracted.")
 
-# print("All zip files in the folder have been extracted.")
-
-# import zipfile, os
-
-folder_path = 'E:/Dataset/'
-[zipfile.ZipFile(os.path.join(folder_path, f), 'r').extractall(folder_path) for f in os.listdir(folder_path) if f.endswith('.zip')]
 print("All zip files in the folder have been extracted.")
